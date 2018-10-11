0e4495ee7bd5eab1f99d9dc52d6a5b002a5c32f2
Adrian Altares <adrianaltares@Adrians-MacBook-Pro.local>
2018-10-11 16:12:59 -0400

    added background, styled buttons, added fonts

---
+++ main.js
@@ -0,0 +1,9 @@
+//create a variable for each letter of the alphabet
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//make an array of words for category
+const words = ["Ford, Toyota, Honda, Dodge, Mercedez, BMW, Tesla"]
+//choose word randomly
+//make underscores equal to length of word
+//if
